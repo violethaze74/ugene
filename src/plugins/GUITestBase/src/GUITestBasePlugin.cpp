82005dfaf263af3ea52ac51d34708ab0c441082b
vmalin <vmalin90@gmail.com>
2016-03-21 17:36:14 +0600

    UGENE-5160 remove gui test

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1254,7 +1254,6 @@ if (QSysInfo::WordSize == 32) {
     REGISTER_TEST_WITH_TIMEOUT(GUITest_regression_scenarios::test_5138_1, 420000);
     REGISTER_TEST(GUITest_regression_scenarios::test_5138_2);
 }
-    REGISTER_TEST(GUITest_regression_scenarios::test_5160);
 
 //////////////////////////////////////////////////////////////////////////
 // Common scenarios/project/
