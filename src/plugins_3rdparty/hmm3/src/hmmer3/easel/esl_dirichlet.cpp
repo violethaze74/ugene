fcf47f90e3801c71e723b17b2df1152faacfe2cd
Denis Kandrov <dendrov.kan@gmail.com>
2016-04-14 17:31:58 +0600

    Fix linux build with old gcc #12

--- src/plugins_3rdparty/hmm3/src/hmmer3/easel/esl_dirichlet.cpp
+++ src/plugins_3rdparty/hmm3/src/hmmer3/easel/esl_dirichlet.cpp
@@ -629,7 +629,7 @@ mixdchlet_complete_func(double *p, int np, void *dptr)
         logP += logPsample;
     }
 
-    if (isnan(logP)) esl_fatal("logP is NaN");
+    if (std::isnan(logP)) esl_fatal("logP is NaN");
     return -logP;
 }
 
@@ -706,12 +706,12 @@ mixdchlet_complete_gradient(double *p, int np, void *dptr, double *dp)
    /* Return the negative, because we're minimizing the NLP, not maximizing.
     */
    for (q = 0; q < nq; q++) {
-        if (isnan(dp[q])) esl_fatal("dp for pq[%d] is NaN", q);
+        if (std::isnan(dp[q])) esl_fatal("dp for pq[%d] is NaN", q);
      dp[q] *= -1.;
    }
    for (q = 0; q < d->N; q++)
      for (x = 0; x < d->K; x++) {
-       if(isnan(dp[nq + q*d->K + x])) esl_fatal("dp for alpha[%d][%d] is NaN", q, x);
+       if(std::isnan(dp[nq + q*d->K + x])) esl_fatal("dp for alpha[%d][%d] is NaN", q, x);
        dp[nq + q*d->K + x] *= -1.0;
     }
 
--- src/plugins_3rdparty/hmm3/src/hmmer3/hmmer3_funcs.h
+++ src/plugins_3rdparty/hmm3/src/hmmer3/hmmer3_funcs.h
@@ -14,6 +14,7 @@ extern float    roundf( float x );
 
 #else // if not Windows -> we have all functions and defines in math.h
 #include <math.h>
+#include <cmath>
 #endif // _WINDOWS
 
 extern float infinity();
