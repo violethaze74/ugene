9310c1df9b54ac207e53695d0cfb92bccb61d107
Denis Kandrov <dendrov.kan@gmail.com>
2016-10-04 13:19:24 +0700

    Copying of libraries was fixed for linux

--- installer/linux/X11/create_bundle.sh
+++ installer/linux/X11/create_bundle.sh
@@ -157,11 +157,11 @@ PATH_TO_MYSQL_CLIENT_LIB=`ldd "${TARGET_APP_DIR}/sqldrivers/libqsqlmysql.so" |gr
 cp -v "$PATH_TO_MYSQL_CLIENT_LIB" "${TARGET_APP_DIR}"
 
 PATH_TO_ICU_DATA_LIB=`ldd "${TARGET_APP_DIR}/libQt5Widgets.so.5" |grep libicudata.so |cut -d " " -f3`
-cp -v "$PATH_TO_ICU_DATA_LIB" "${TARGET_APP_DIR}"
+cp -v -L "$PATH_TO_ICU_DATA_LIB" "${TARGET_APP_DIR}"
 PATH_TO_ICU_I18N_LIB=`ldd "${TARGET_APP_DIR}/libQt5Widgets.so.5" |grep libicui18n.so |cut -d " " -f3`
-cp -v "$PATH_TO_ICU_I18N_LIB" "${TARGET_APP_DIR}"
+cp -v -L "$PATH_TO_ICU_I18N_LIB" "${TARGET_APP_DIR}"
 PATH_TO_ICU_UUC_LIB=`ldd "${TARGET_APP_DIR}/libQt5Widgets.so.5" |grep libicuuc.so |cut -d " " -f3`
-cp -v "$PATH_TO_ICU_UUC_LIB" "${TARGET_APP_DIR}"
+cp -v -L "$PATH_TO_ICU_UUC_LIB" "${TARGET_APP_DIR}"
 
 if [ "$1" == "-test" ]
     then
