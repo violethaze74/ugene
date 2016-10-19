187c4255c5f091fca11e7124df705e87a9115042
Kirill Rasputin <rasputinkirill@gmail.com>
2016-10-19 16:09:48 +0700

    UGENE-5412 "Paired reads have different names" error for "Raw DNA-Seq data processing" workflow dashboard message was updated

--- src/corelibs/U2View/src/util_dna_assembly/DnaAssemblyUtils.cpp
+++ src/corelibs/U2View/src/util_dna_assembly/DnaAssemblyUtils.cpp
@@ -321,9 +321,9 @@ void FilterUnpairedReadsTask::compareFiles(const GUrl &upstream, const GUrl &dow
     CHECK_OP(stateInfo, );
 
     if (comparator.getUnpairedCount() != 0) {
-        stateInfo.addWarning(tr("%1 pairs are complete, %2 reads without a pair were found in files %3 and %4.")
+        stateInfo.addWarning(tr("%1 read pairs were mapped, %2 reads without a pair from files \"%3\" and \"%4\" were skipped.")
                              .arg(comparator.getPairsCount()).arg(comparator.getUnpairedCount())
-                             .arg(upstream.getURLString()).arg(downstream.getURLString()));
+                             .arg(QFileInfo(upstream.getURLString()).fileName()).arg(QFileInfo(downstream.getURLString()).fileName()));
     }
 }
 
