bd5d0778698615755530dfd91aebe5bbc6a4f190
atiunov <atiunov@unipro.ru>
2016-12-07 14:49:15 +0700

    UGENE-4775: mac build fix

--- src/plugins/external_tool_support/src/blast_plus/BlastDBCmdDialog.cpp
+++ src/plugins/external_tool_support/src/blast_plus/BlastDBCmdDialog.cpp
@@ -40,11 +40,6 @@ BlastDBCmdDialog::BlastDBCmdDialog(BlastDBCmdSupportTaskSettings &_settings, QWi
     setupUi(this);
     new HelpButton(this, buttonBox, "18223228");
 
-#ifdef Q_OS_MAC
-    // Add a spacer between 'Query ID' input field and 'BLAST DB' groupbox
-    verticalLayout_2->insertSpacerItem(verticalLayout_2->indexOf(dbSelectorWidget), new QSpacerItem(1, 12));
-#endif
-
     buttonBox->button(QDialogButtonBox::Ok)->setText(tr("Fetch"));
     buttonBox->button(QDialogButtonBox::Cancel)->setText(tr("Cancel"));
 
