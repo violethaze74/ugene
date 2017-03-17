c2becbb6ce6ca8304ee057bfcf3c139dce5c6047
smlnkcv <s.samoilenko@unipro.ru>
2017-03-17 13:03:48 +0700

    UGENE 5199: update gui test, add Sleep

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_5001_6000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_5001_6000.cpp
@@ -546,6 +546,7 @@ GUI_TEST_CLASS_DEFINITION(test_5199) {
             GTUtilsTaskTreeView::waitTaskFinished(os);
 
             QTableWidget *resultsTable = GTWidget::findExactWidget<QTableWidget *>(os, "resultsTable", dialog);
+            GTGlobals::sleep();
             CHECK_SET_ERR(NULL != resultsTable, "resultsTable is NULL");
             const int resultsCount = resultsTable->rowCount();
             CHECK_SET_ERR(4 == resultsCount, QString("Unexpected results count: expected %1, got %2").arg(4).arg(resultsCount));
