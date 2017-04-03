369ab8ec3bbdc3f82b92a2bbbf150f4dd15deb79
smlnkcv <s.samoilenko@unipro.ru>
2017-04-03 17:49:25 +0700

    UGENE-2972: new dialog name "PhmmerSearchDialog"

--- src/plugins/GUITestBase/src/runnables/ugene/plugins_3rdparty/hmm3/UHMM3PhmmerDialogFiller.h
+++ src/plugins/GUITestBase/src/runnables/ugene/plugins_3rdparty/hmm3/UHMM3PhmmerDialogFiller.h
@@ -30,7 +30,7 @@ using namespace HI;
 class UHMM3PhmmerDialogFiller: public Filler
 {
 public:
-    UHMM3PhmmerDialogFiller(HI::GUITestOpStatus &os, QString _input):Filler(os, "UHMM3PhmmerDialog"), input(_input){}
+    UHMM3PhmmerDialogFiller(HI::GUITestOpStatus &os, QString _input):Filler(os, "PhmmerSearchDialog"), input(_input){}
     void commonScenario();
 private:
     QString input;
