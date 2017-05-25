2e8ae0862204de1dad2fbe487d97df4d1d0b3407
Kirill Rasputin <rasputinkirill@gmail.com>
2017-05-25 13:48:01 +0700

    UGENE-5587 Selection status hidden in Sanger view

--- src/corelibs/U2View/src/ov_msa/MSAEditorStatusBar.cpp
+++ src/corelibs/U2View/src/ov_msa/MSAEditorStatusBar.cpp
@@ -91,7 +91,9 @@ MSAEditorStatusWidget::MSAEditorStatusWidget(MultipleAlignmentObject* mobj, MaEd
     l->addWidget(linesLabel);
     l->addWidget(colsLabel);
     l->addWidget(posLabel);
-    l->addWidget(selectionLabel);
+    if (qobject_cast<MSAEditor*>(seqArea->getEditor()) != NULL) {
+        l->addWidget(selectionLabel);
+    }
     l->addWidget(lockLabel);
     setLayout(l);
 
