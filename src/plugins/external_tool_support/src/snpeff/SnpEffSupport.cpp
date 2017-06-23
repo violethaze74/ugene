7b78d2196c36453dee0f46b988c6864dfe149f7d
atiunov <atiunov@unipro.ru>
2017-06-23 20:00:10 +0700

    UGENE-5654: SnpEff version is detected incorrectly

--- src/plugins/external_tool_support/src/snpeff/SnpEffSupport.cpp
+++ src/plugins/external_tool_support/src/snpeff/SnpEffSupport.cpp
@@ -52,7 +52,7 @@ SnpEffSupport::SnpEffSupport(const QString& name, const QString& path) : Externa
     validMessage = "Usage: snpEff \\[command\\] \\[options\\] \\[files\\]";
     description = tr("<i>SnpEff</i>: Genetic variant annotation and effect prediction toolbox.");
 
-    versionRegExp = QRegExp("version SnpEff (\\d+.\\d+)");
+    versionRegExp = QRegExp("version SnpEff (\\d+.\\d+[a-zA-Z]?)");
     validationArguments << "-h";
     toolKitName = "SnpEff";
 
