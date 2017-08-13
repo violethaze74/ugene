56d255c8adcbdb8a1c4625523d29179c38fc8dd1
Yuliya Algaer <yalgaer@gmail.com>
2017-08-13 10:00:00 +0700

    Doc ids update

--- src/corelibs/U2View/src/phyltree/TextSettingsDialog.cpp
+++ src/corelibs/U2View/src/phyltree/TextSettingsDialog.cpp
@@ -33,7 +33,7 @@ TextSettingsDialog::TextSettingsDialog(QWidget *parent, const OptionsMap& settin
 : BaseSettingsDialog(parent) {
 
     setupUi(this);
-    new HelpButton(this, buttonBox, "19759632");
+    new HelpButton(this, buttonBox, "19766888");
     buttonBox->button(QDialogButtonBox::Ok)->setText(tr("OK"));
     buttonBox->button(QDialogButtonBox::Cancel)->setText(tr("Cancel"));
 
