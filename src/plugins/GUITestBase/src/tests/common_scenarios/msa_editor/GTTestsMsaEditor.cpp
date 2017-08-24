ca9eccdfa7e1f48bcd5ee38d0d77097c8f07d6a6
smlnkcv <s.samoilenko@unipro.ru>
2017-08-24 13:28:35 +0700

    Corrected msa_editor_0056 for stability

--- src/plugins/GUITestBase/src/tests/common_scenarios/msa_editor/GTTestsMsaEditor.cpp
+++ src/plugins/GUITestBase/src/tests/common_scenarios/msa_editor/GTTestsMsaEditor.cpp
@@ -3997,7 +3997,7 @@ GUI_TEST_CLASS_DEFINITION(test_0056){
                                                   << "export sequences as alignment"));
     GTUtilsProjectTreeView::click(os, "murine.gb", Qt::RightButton);
     GTGlobals::sleep();
-    GTGlobals::sleep();
+	GTUtilsMdi::activateWindow(os, "murine [m] murine");
 //    "Use Genbank "SOURCE" tags..." checkbox
     QStringList nameList = GTUtilsMSAEditorSequenceArea::getNameList(os);
     CHECK_SET_ERR(nameList.size() == 1, QString("unexpected number of names: %1").arg(nameList.size()));
