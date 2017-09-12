37ad87e76cae6e21c8db58e63f2b4d6ffc5d04eb
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2017-09-12 15:51:51 +0700

    MCA GUI Test UGENE-5758 Collapsing model is not updated on the undo/redo in the Mca Editor

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1336,6 +1336,7 @@ void GUITestBasePlugin::registerTests(UGUITestBase *guiTestBase) {
     REGISTER_TEST(GUITest_regression_scenarios::test_5752);
     REGISTER_TEST(GUITest_regression_scenarios::test_5753);
     REGISTER_TEST(GUITest_regression_scenarios::test_5755);
+    REGISTER_TEST(GUITest_regression_scenarios::test_5758);
 
     //////////////////////////////////////////////////////////////////////////
     // Common scenarios/project/
