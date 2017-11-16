8e1a440f3894d65fce3d2d1fadecdcdd6003f474
atiunov <atiunov@unipro.ru>
2017-11-16 14:12:30 +0700

    GUI tests fix

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_5001_6000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_5001_6000.cpp
@@ -2881,18 +2881,18 @@ GUI_TEST_CLASS_DEFINITION(test_5739) {
     GTUtilsTaskTreeView::waitTaskFinished(os);
 
     //5. Click to the position 6316 at the reference
-    GTUtilsMcaEditorSequenceArea::clickToReferencePosition(os, 6376);
+    GTUtilsMcaEditorSequenceArea::clickToReferencePosition(os, 6372);
 
     //6. Select all chars in the reference from here to the end
     QPoint currentPos = GTMouseDriver::getMousePosition();
-    QPoint destPos(currentPos.x() + 100, currentPos.y());
+    const int newXPos = GTUtilsMdi::activeWindow(os)->mapToGlobal(GTUtilsMdi::activeWindow(os)->rect().topRight()).x();
+    QPoint destPos(newXPos, currentPos.y());
     GTUtilsMcaEditorSequenceArea::dragAndDrop(os, destPos);
 
     //Expected: selected length = 4
     U2Region reg = GTUtilsMcaEditorSequenceArea::getReferenceSelection(os);
     int sel = reg.length;
-    CHECK_SET_ERR(sel == 4, QString("Unexpected selection length, expectedL 4, ceurrent: %1").arg(QString::number(sel)));
-
+    CHECK_SET_ERR(sel == 4, QString("Unexpected selection length, expectedL 4, current: %1").arg(QString::number(sel)));
 }
 
 GUI_TEST_CLASS_DEFINITION(test_5747) {
