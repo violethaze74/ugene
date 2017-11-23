e364ddd73e466fd2d5e2b3c17d4f45d8f1427a52
Yuliya Algaer <yalgaer@gmail.com>
2017-11-23 09:00:00 +0700

    Translations update

--- src/corelibs/U2Algorithm/transl/russian.ts
+++ src/corelibs/U2Algorithm/transl/russian.ts
@@ -53,7 +53,7 @@
         <translation>Найдены ошибки в библиотеке: %1</translation>
     </message>
     <message>
-        <location filename="../src/util_msa_consensus/MSAConsensusUtils.cpp" line="108"/>
+        <location filename="../src/util_msa_consensus/MSAConsensusUtils.cpp" line="111"/>
         <source>Gaps:</source>
         <translation>Пробелы:</translation>
     </message>
