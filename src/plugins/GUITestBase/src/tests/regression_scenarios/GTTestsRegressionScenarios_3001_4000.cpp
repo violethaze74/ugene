4009f6c356261c05306cfe277b8c20b2187ba731
smlnkcv <s.samoilenko@unipro.ru>
2018-05-16 17:54:04 +0700

    Corrected gui_3085

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_3001_4000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_3001_4000.cpp
@@ -542,6 +542,7 @@ GUI_TEST_CLASS_DEFINITION(test_3085_1) {
 
     //Expected state: file was updated, the sequence view with annotations is opened and updated.
     QWidget *reloaded1Sv = GTUtilsMdi::activeWindow(os);
+    GTGlobals::sleep(1000);
     CHECK_SET_ERR(sv != reloaded1Sv, "File is not reloaded 1");
 
     //4. Change the annotations file outside UGENE (e.g. change annotation region).
@@ -570,7 +571,7 @@ GUI_TEST_CLASS_DEFINITION(test_3085_2) {
     GTUtilsDialog::waitForDialog(os, new MessageBoxDialogFiller(os, QMessageBox::Yes));
     QFile(sandBoxDir + "murine_3085_2.gb").rename(sandBoxDir + "murine_3085_2_1.gb");
     QFile(testDir + "_common_data/regression/3085/test_1.gb").copy(sandBoxDir + "murine_3085_2.gb");
-    GTGlobals::sleep(5000);
+    GTGlobals::sleep(10000);
 
     //Expected state: document reloaded without errors/warnings.
     CHECK_SET_ERR(!l.hasError(), "Errors in log");
