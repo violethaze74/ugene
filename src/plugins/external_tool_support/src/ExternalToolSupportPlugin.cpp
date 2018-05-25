9704af21263101e1bac0323da8983faa0d420780
Olga Golosova <oigl@oigl-ntbk1.unipro.ru>
2018-05-25 18:03:44 +0700

    UGENE-6092: integrated Trimmomatic.

--- src/plugins/external_tool_support/src/ExternalToolSupportPlugin.cpp
+++ src/plugins/external_tool_support/src/ExternalToolSupportPlugin.cpp
@@ -142,6 +142,7 @@
 #include "tcoffee/TCoffeeWorker.h"
 #include "tophat/TopHatSupport.h"
 #include "tophat/TopHatWorker.h"
+#include "trimmomatic/TrimmomaticSupport.h"
 #include "utils/ExternalToolSupportAction.h"
 #include "utils/ExternalToolValidateTask.h"
 #include "vcftools/VcfConsensusSupport.h"
@@ -392,7 +393,6 @@ ExternalToolSupportPlugin::ExternalToolSupportPlugin() :
     BigWigSupport* bigwigSupport = new BigWigSupport(ET_BIGWIG);
     etRegistry->registerEntry(bigwigSupport);
 
-
     // TopHat
     TopHatSupport* tophatTool = new TopHatSupport(ET_TOPHAT);
     etRegistry->registerEntry(tophatTool);
@@ -445,9 +445,14 @@ ExternalToolSupportPlugin::ExternalToolSupportPlugin() :
     FastQCSupport *fastqc = new FastQCSupport(ET_FASTQC);
     etRegistry->registerEntry(fastqc);
 
+    //HMMER
     etRegistry->registerEntry(new HmmerSupport(HmmerSupport::BUILD_TOOL));
     etRegistry->registerEntry(new HmmerSupport(HmmerSupport::SEARCH_TOOL));
     etRegistry->registerEntry(new HmmerSupport(HmmerSupport::PHMMER_TOOL));
+
+    //Trimmomatic
+    TrimmomaticSupport *trimmomaticSupport = new TrimmomaticSupport(ET_TRIMMOMATIC);
+    etRegistry->registerEntry(trimmomaticSupport);
 
     if (AppContext::getMainWindow()) {
 
