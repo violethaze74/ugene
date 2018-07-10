d3fae077e701e55524a5a1cc479a537633902e68
Kirill Rasputin <rasputinkirill@gmail.com>
2018-07-10 18:43:18 +0700

    UGENE-6123 SPAdes workflow element output data

--- src/corelibs/U2Algorithm/src/misc/GenomeAssemblyMultiTask.cpp
+++ src/corelibs/U2Algorithm/src/misc/GenomeAssemblyMultiTask.cpp
@@ -90,6 +90,10 @@ QList<Task*> GenomeAssemblyMultiTask::onSubTaskFinished( Task* subTask ) {
 }
 
 
+U2::GenomeAssemblyTask* GenomeAssemblyMultiTask::getAssemblyTask() const {
+    return assemblyTask;
+}
+
 QString GenomeAssemblyMultiTask::generateReport() const {
     QString res;
     if (hasError()) {
@@ -104,13 +108,11 @@ QString GenomeAssemblyMultiTask::generateReport() const {
     return res;
 }
 
-QString GenomeAssemblyMultiTask::getResultUrl() const{
+QString GenomeAssemblyMultiTask::getResultUrl() const {
     if(assemblyTask && assemblyTask->isFinished() && !assemblyTask->hasError()){
         return assemblyTask->getResultUrl();
     }
     return "";
 }
-
-
 
 } // namespace
