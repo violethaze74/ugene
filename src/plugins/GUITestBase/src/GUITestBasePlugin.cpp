5b23826376b31455209e9517e4edbf47d895efb0
Yuliya Algaer <yalgaer@gmail.com>
2018-12-12 03:55:46 +0700

    UGENE-4483. GUI test

--- src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
+++ src/plugins/GUITestBase/src/GUITestBasePlugin.cpp
@@ -1153,6 +1153,7 @@ void GUITestBasePlugin::registerTests(UGUITestBase *guiTestBase) {
     REGISTER_TEST(GUITest_regression_scenarios::test_4439);
     REGISTER_TEST(GUITest_regression_scenarios::test_4440);
     REGISTER_TEST(GUITest_regression_scenarios::test_4463);
+    REGISTER_TEST(GUITest_regression_scenarios::test_4483);
     REGISTER_TEST(GUITest_regression_scenarios::test_4486);
     REGISTER_TEST(GUITest_regression_scenarios::test_4488);
     REGISTER_TEST(GUITest_regression_scenarios::test_4489);
