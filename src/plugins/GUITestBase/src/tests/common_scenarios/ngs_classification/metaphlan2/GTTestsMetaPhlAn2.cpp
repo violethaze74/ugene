2639737990a203740d128f896a04a0871a9ed1d5
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2019-01-30 12:57:07 +0700

    UGENE-6311 metagenomic GUI tests fix

--- src/plugins/GUITestBase/src/tests/common_scenarios/ngs_classification/metaphlan2/GTTestsMetaPhlAn2.cpp
+++ src/plugins/GUITestBase/src/tests/common_scenarios/ngs_classification/metaphlan2/GTTestsMetaPhlAn2.cpp
@@ -486,14 +486,6 @@ GUI_TEST_CLASS_DEFINITION(test_0001) {
                               .arg(row));
     }
 
-    {//6. Check "Input file format"
-        //Expected: current "Input file format" value is "FASTA"
-        QString inputFileFormat = GTUtilsWorkflowDesigner::getParameter(os, INPUT_FILE_FORMAT);
-        CHECK_SET_ERR(inputFileFormat == INPUT_FILE_FORMAT_VALUES.first(),
-                      QString("Unexpected \"Input file format\" value, expected: FASTA, current: %1")
-                              .arg(inputFileFormat));
-    }
-
     {//7. Check "Database"
         //Expected: database path ends with 'data/ngs_classification/metaphlan2/mpa_v20_m200'
         QString databasePath = QDir::toNativeSeparators(GTUtilsWorkflowDesigner::getParameter(os, DATABASE));
@@ -626,22 +618,6 @@ GUI_TEST_CLASS_DEFINITION(test_0002) {
             CHECK_SET_ERR(inputDataValues.contains(value),
                 QString("Input data doesn't contain %1 value, but should be")
                         .arg(value));
-        }
-    }
-
-    {
-        //9. Check if "Input file format" parameter has 2 values:
-        //FASTA, FASTQ
-        QStringList inputFileFormatValues = GTUtilsWorkflowDesigner::getComboBoxParameterValues(os, INPUT_FILE_FORMAT);
-        CHECK_SET_ERR(inputFileFormatValues.size() == INPUT_FILE_FORMAT_VALUES.size(),
-            QString("Unexpected \"Input file format\" values size, expected: %1, current: %2")
-                    .arg(INPUT_FILE_FORMAT_VALUES.size())
-                    .arg(inputFileFormatValues.size()));
-
-        foreach(const QString& value, INPUT_FILE_FORMAT_VALUES) {
-            CHECK_SET_ERR(inputFileFormatValues.contains(value),
-                          QString("Input file format doesn't contain %1 value, but should be")
-                                  .arg(value));
         }
     }
 }
