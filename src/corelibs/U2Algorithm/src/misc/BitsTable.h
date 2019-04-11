bcf2a00c003b13e93201f48e8e01ba706e9ee059
atiunov <atiunov@unipro.ru>
2019-04-11 13:08:45 +0700

    UTI-282 (reverted from commit 0957e0261701427112fba10d7c48a71977877fe4)

--- src/corelibs/U2Algorithm/src/misc/BitsTable.h
+++ src/corelibs/U2Algorithm/src/misc/BitsTable.h
@@ -26,14 +26,23 @@
 
 namespace U2 {
 
-class U2ALGORITHM_EXPORT BitsTable {
+class U2ALGORITHM_EXPORT BitsTable  {
 public:
+    quint32 NUCL_BITS[256];
+    const static quint32 NUCL_BITS_SIZE;
+
+    quint32 PROTEIN_BITS[256];
+    const static quint32 PROTEIN_BITS_SIZE;
+
+    quint32 RAW_BITS[256];
+    const static quint32 RAW_BITS_SIZE;
 
     BitsTable();
+    virtual ~BitsTable(){}
 
     quint32 getBitMaskCharBitsNum(DNAAlphabetType alType) const;
-
     const quint32* getBitMaskCharBits(DNAAlphabetType alType) const;
+
 };
 
 } //namespace
