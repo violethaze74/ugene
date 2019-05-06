c8eced0372095e82062c3f84bd9e90dd949b69e2
Kirill Rasputin <rasputinkirill@gmail.com>
2019-05-06 15:04:54 +0700

    UGENE-6406 GTF parsing errors for chr20.gtf

--- src/corelibs/U2Formats/src/GTFFormat.h
+++ src/corelibs/U2Formats/src/GTFFormat.h
@@ -56,8 +56,6 @@ public:
             incorrectScore ||
             incorrectStrand ||
             incorrectFrame ||
-            noGeneIdAttribute ||
-            noTranscriptIdAttribute ||
             incorrectFormatOfAttributes;
     }
 
