55b59378afd09436be9679a89d1baf48710eaaa4
Kirill Rasputin <rasputinkirill@gmail.com>
2019-06-06 12:53:44 +0700

    UGENE-6435 Add statistic counters to "element with command line tool" mechanism

--- src/plugins/workflow_designer/src/WorkflowViewController.cpp
+++ src/plugins/workflow_designer/src/WorkflowViewController.cpp
@@ -980,8 +980,13 @@ void WorkflowView::addProcess(Actor *proc, const QPointF &pos) {
     if (NULL != g || NULL != m) {
         connect(editor, SIGNAL(si_configurationChanged()), scene, SLOT(sl_refreshBindings()));
     }
-
     sl_procItemAdded();
+    ActorPrototype *addedProto = it->getProcess()->getProto();
+    uiLog.trace(addedProto->getDisplayName() + " added");
+    if (WorkflowEnv::getExternalCfgRegistry()->getConfigById(addedProto->getId()) != nullptr) {
+        GCOUNTER(cvar, tvar, "Element with command line tool is added to the scene");
+    }
+
     update();
 }
 
