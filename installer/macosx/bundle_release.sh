c8e3ba300cf2870b3a30ca8c8c18c05a225aca6d
Kirill Rasputin <rasputinkirill@gmail.com>
2019-06-26 19:07:16 +0700

    UGENE-6486 Modify UGENE versions numeration

--- installer/macosx/bundle_release.sh
+++ installer/macosx/bundle_release.sh
@@ -7,11 +7,9 @@ echo Source: $SOURCE_DIR
 
 VERSION_MAJOR=`cat ${SOURCE_DIR}/src/ugene_version.pri | grep 'UGENE_VER_MAJOR=' | awk -F'=' '{print $2}'`
 VERSION_MINOR=`cat ${SOURCE_DIR}/src/ugene_version.pri | grep 'UGENE_VER_MINOR=' | awk -F'=' '{print $2}'`
-VERSION_PATCH=`cat ${SOURCE_DIR}/src/ugene_version.pri | grep 'UGENE_VER_PATCH=' | awk -F'=' '{print $2}'`
 UGENE_VERSION=`cat ${SOURCE_DIR}/src/ugene_version.pri | grep UGENE_VERSION | awk -F'=' '{print $2}' | \
                sed -e 's/$${UGENE_VER_MAJOR}/'"$VERSION_MAJOR"'/g' \
-                   -e 's/$${UGENE_VER_MINOR}/'"$VERSION_MINOR"'/g' \
-                   -e 's/$${UGENE_VER_PATCH}/'"$VERSION_PATCH"'/g'`
+                   -e 's/$${UGENE_VER_MINOR}/'"$VERSION_MINOR"'/g'`
 
 ARCHITECTURE=`uname -m`
 BUILD_DIR=./release_bundle
