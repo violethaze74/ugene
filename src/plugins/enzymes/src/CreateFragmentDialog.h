1bd1ed9b8936d1fd05c5c4b4ceb3ba32aca847fe
atiunov <atiunov@unipro.ru>
2020-01-13 17:08:18 +0700

    Issue #50: Qt5.14.0 build fix

--- src/plugins/enzymes/src/CreateFragmentDialog.h
+++ src/plugins/enzymes/src/CreateFragmentDialog.h
@@ -22,6 +22,7 @@
 #ifndef _U2_CREATE_FRAGMENT_DIALOG_H_
 #define _U2_CREATE_FRAGMENT_DIALOG_H_
 
+#include <QSet>
 #include "DNAFragment.h"
 #include <U2Gui/RegionSelector.h>
 #include <ui_CreateFragmentDialog.h>
