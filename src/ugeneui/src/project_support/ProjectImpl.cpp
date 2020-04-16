a991912fcd26abcb5e901d9ba82b0f6b4175edac
Kirill Rasputin <rasputinkirill@gmail.com>
2020-04-16 23:48:14 +0700

    Build fix

--- src/ugeneui/src/project_support/ProjectImpl.cpp
+++ src/ugeneui/src/project_support/ProjectImpl.cpp
@@ -217,7 +217,7 @@ void ProjectImpl::sl_onObjectRemoved(GObject* obj) {
 
 void ProjectImpl::sl_onObjectRelationChanged(const QList<GObjectRelation> &previousRelations) {
     GObject *obj = qobject_cast<GObject *>(sender());
-    CHECK(obj != nullptr)
+    CHECK(obj != nullptr, )
     QSet<GObjectRelation> relationsSet = obj->getObjectRelations().toSet();
     relationsSet.unite(previousRelations.toSet());
     QList<GObject *> allObjs;
