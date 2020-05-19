b8bb673ff4554486227d3bcfac4342ee3bf74173
Yuliya Algaer <yalgaer@gmail.com>
2020-05-19 19:17:27 -0400

    Increasing tests timeout on Linux

--- src/corelibs/U2Test/src/gui_tests/GUITestLauncher.cpp
+++ src/corelibs/U2Test/src/gui_tests/GUITestLauncher.cpp
@@ -39,7 +39,7 @@
 #include "UGUITestBase.h"
 
 #ifdef Q_OS_LINUX
-#    define GUI_TEST_TIMEOUT_MILLIS 240000
+#    define GUI_TEST_TIMEOUT_MILLIS 300000
 #else
 #    define GUI_TEST_TIMEOUT_MILLIS 480000
 #endif
