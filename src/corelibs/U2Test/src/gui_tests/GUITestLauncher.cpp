52b419e8bd08688bf70e1f3653ba32ee08d947d2
Yuliya Algaer <yalgaer@gmail.com>
2020-06-26 16:28:19 -0400

    Fixing ideal test count per suite on Linux

--- src/corelibs/U2Test/src/gui_tests/GUITestLauncher.cpp
+++ src/corelibs/U2Test/src/gui_tests/GUITestLauncher.cpp
@@ -148,7 +148,7 @@ QList<HI::GUITest *> getIdealTestsSplit(int suiteIndex, int suiteCount, const QL
     } else if (suiteCount == 4) {
         testsPerSuite << 640 << 680 << 640 << -1;
     } else if (suiteCount == 5) {
-        testsPerSuite << 518 << 563 << 484 << 515 << -1;
+        testsPerSuite << 518 << 563 << 484 << 521 << -1;
     }
     QList<HI::GUITest *> tests;
     if (testsPerSuite.size() == suiteCount) {
