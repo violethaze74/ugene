357151f7600fa6ceb45c3b8609d62fb262c36497
Yuliya Algaer <yalgaer@gmail.com>
2020-07-04 22:39:08 -0400

    Fix for GUITest_regression_scenarios_test_5137

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_5001_6000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_5001_6000.cpp
@@ -601,17 +601,18 @@ GUI_TEST_CLASS_DEFINITION(test_5130) {
 }
 
 GUI_TEST_CLASS_DEFINITION(test_5137) {
-    //    1. Open document test/_common_data/clustal/big.aln
+    // 1. Open document test/_common_data/clustal/big.aln
     GTFileDialog::openFile(os, dataDir + "samples/CLUSTALW", "COI.aln");
     GTUtilsMsaEditor::checkMsaEditorWindowIsActive(os);
     GTUtilsTaskTreeView::waitTaskFinished(os);
 
-    //    2. Add big sequence
+    // 2. Add a big sequence.
     GTUtilsDialog::waitForDialog(os, new GTFileDialogUtils(os, testDir + "_common_data/fasta/", "PF07724_full_family.fa"));
-    QAbstractButton *align = GTAction::button(os, "Align sequence(s) to this alignment");
-    CHECK_SET_ERR(align != NULL, "MSA \"Align sequence(s) to this alignment\" action not found");
-    GTWidget::click(os, align);
+    QAbstractButton *alignButton = GTAction::button(os, "Align sequence(s) to this alignment");
+    CHECK_SET_ERR(alignButton != nullptr, "MSA \"Align sequence(s) to this alignment\" action not found");
+    GTWidget::click(os, alignButton);
 
+    // 3. Delete the original alignment and wait for the error in the log.
     GTUtilsNotifications::waitForNotification(os, true, "A problem occurred during adding sequences. The multiple alignment is no more available.");
     GTUtilsProjectTreeView::click(os, "COI");
     GTKeyboardDriver::keyClick(Qt::Key_Delete);
