facd0993e87b498a8e61adce1d84268e73432a81
Yuliya Algaer <yalgaer@gmail.com>
2020-07-28 13:52:44 -0400

    Fix for scanning_0007 gui test

--- src/plugins/GUITestBase/src/tests/common_scenarios/workflow_designer/dashboard/GTTestsWorkflowDashboard.cpp
+++ src/plugins/GUITestBase/src/tests/common_scenarios/workflow_designer/dashboard/GTTestsWorkflowDashboard.cpp
@@ -4185,7 +4185,7 @@ GUI_TEST_CLASS_DEFINITION(output_dir_scanning_test_0007) {
     GTMouseDriver::click();
 
     //    7. Add "_common_data/clustal/200_sequences.aln" file to "Dataset 1" dataset.
-    GTUtilsWorkflowDesigner::setDatasetInputFile(os, testDir + "_common_data/clustal/200_sequences.aln");
+    GTUtilsWorkflowDesigner::setDatasetInputFile(os, testDir + "_common_data/clustal/100_sequences.aln");
 
     //    8. Open Workflow Designer.
     GTUtilsWorkflowDesigner::openWorkflowDesigner(os);
@@ -4203,7 +4203,7 @@ GUI_TEST_CLASS_DEFINITION(output_dir_scanning_test_0007) {
     GTMouseDriver::click();
 
     //    12. Add "_common_data/clustal/200_sequences.aln" file to "Dataset 1" dataset.
-    GTUtilsWorkflowDesigner::setDatasetInputFile(os, testDir + "_common_data/clustal/200_sequences.aln");
+    GTUtilsWorkflowDesigner::setDatasetInputFile(os, testDir + "_common_data/clustal/100_sequences.aln");
 
     //    13. Open Workflow Designer.
     GTUtilsWorkflowDesigner::openWorkflowDesigner(os);
