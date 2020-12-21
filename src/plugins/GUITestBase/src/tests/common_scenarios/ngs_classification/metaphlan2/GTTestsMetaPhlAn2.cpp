578c3bed2d13f12148deb43323ebc5861e7b8d9b
DmitriiSukhomlinov <sukhomlinovdmitrii@yandex.ru>
2020-12-21 18:05:12 +0700

    Metagenomics GUI tes fix

--- src/plugins/GUITestBase/src/tests/common_scenarios/ngs_classification/metaphlan2/GTTestsMetaPhlAn2.cpp
+++ src/plugins/GUITestBase/src/tests/common_scenarios/ngs_classification/metaphlan2/GTTestsMetaPhlAn2.cpp
@@ -440,7 +440,7 @@ GUI_TEST_CLASS_DEFINITION(test_0008) {
     QStringList errors = GTUtilsWorkflowDesigner::getErrors(os);
     QString error("Classify Sequences with MetaPhlAn2: External tool \"MetaPhlAn2\" is invalid. UGENE may not support this version of the tool or a wrong path to the tools is selected");
     CHECK_SET_ERR(errors.contains(error), "The expected error is absent");
-    const int expectedErrorCount = 3;
+    const int expectedErrorCount = 2;
     CHECK_SET_ERR(expectedErrorCount == errors.size(), QString("There are too many errors: expected %1, got %2").arg(expectedErrorCount).arg(errors.size()));
 }
 
