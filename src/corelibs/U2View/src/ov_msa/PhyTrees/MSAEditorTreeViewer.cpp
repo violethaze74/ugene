0920cc61c500b3f9c07d8d5bc063d48951b04c86
Yuliya Algaer <yalgaer@gmail.com>
2021-02-17 16:00:46 -0500

    UGENE-7053. Assert: 'invalid comparator' on building tree

--- src/corelibs/U2View/src/ov_msa/PhyTrees/MSAEditorTreeViewer.cpp
+++ src/corelibs/U2View/src/ov_msa/PhyTrees/MSAEditorTreeViewer.cpp
@@ -688,7 +688,7 @@ QList<QStringList> MSAEditorTreeViewerUI::getGroupingStateForMsa() const {
         // Sort items by Y, so virtual order will be the same with the tree.
         // The item with the higher Y must go first: this way it will be processed last when pushed to the stack below.
         std::sort(childItemList.begin(), childItemList.end(), [](const QGraphicsItem *item1, const QGraphicsItem *item2) {
-            return item1->y() >= item2->y();
+            return item1->y() > item2->y();
         });
 
         for (QGraphicsItem *childItem : qAsConst(childItemList)) {
