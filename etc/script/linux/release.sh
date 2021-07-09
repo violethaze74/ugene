18532cf6c1ccfb1e226e32975124c2641e1433fa
Yuliya Algaer <yalgaer@gmail.com>
2021-07-09 17:27:40 -0400

    Fix release.sh for Linux

--- etc/script/linux/release.sh
+++ etc/script/linux/release.sh
@@ -82,7 +82,7 @@ function dump_symbols() {
   mv "${SYMBOL_FILE}" "${DEST_PATH}/${FILE_NAME}.sym"
 }
 
-find "${APP_DIR}" | sed 's/.*\/tools\/.*$//g' | grep -e ugeneui -e ugenecl -e lib.*.so.* | while read -r BINARY_FILE; do
+find "${APP_BUNDLE_DIR}" | sed 's/.*\/tools\/.*$//g' | grep -e ugeneui -e ugenecl -e lib.*.so.* | while read -r BINARY_FILE; do
   dump_symbols "${BINARY_FILE}"
 done
 echo "##teamcity[blockClosed name='Dump symbols']"
