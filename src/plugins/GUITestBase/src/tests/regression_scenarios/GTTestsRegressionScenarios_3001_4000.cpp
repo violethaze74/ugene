c2bf04ad677306b0a8355c93518756c8b5d74e50
Kirill Rasputin <rasputinkirill@gmail.com>
2021-12-02 11:49:57 +0700

    UGENE-3200 Project opens on project closing (#617)

--- src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_3001_4000.cpp
+++ src/plugins/GUITestBase/src/tests/regression_scenarios/GTTestsRegressionScenarios_3001_4000.cpp
@@ -1065,8 +1065,7 @@ GUI_TEST_CLASS_DEFINITION(test_3165) {
     //     Expected state: UGENE notices that it can't rewrite the file, it offers you to save the file to another location.
     //     7. Save file anywhere.
     //     Expected state: the project closes, the file is successfully saved, UGENE doesn't crash.
-    // TODO: add this check after UGENE-3200 fix
-    // GTUtilsProject::checkProject(os, GTUtilsProject::NotExists);
+    GTUtilsProject::checkProject(os, GTUtilsProject::NotExists);
     CHECK_SET_ERR(GTFile::check(os, sandBoxDir + "test_3165_out.aln"), "file not saved");
     //    Current state: file is successfully saved, then UGENE crashes.
 }
--- src/ugeneui/src/project_support/ProjectTasksGui.cpp
+++ src/ugeneui/src/project_support/ProjectTasksGui.cpp
@@ -194,7 +194,7 @@ void SaveProjectTask::prepare() {
     if (k != SaveProjectTaskKind_SaveProjectOnly) {
         QList<Document *> modifiedDocs = SaveMultipleDocuments::findModifiedDocuments(AppContext::getProject()->getDocuments());
         if (!modifiedDocs.isEmpty()) {
-            ssTasks.append(new SaveMultipleDocuments(modifiedDocs, k == SaveProjectTaskKind_SaveProjectAndDocumentsAskEach, SavedNewDoc_Open));
+            ssTasks.append(new SaveMultipleDocuments(modifiedDocs, k == SaveProjectTaskKind_SaveProjectAndDocumentsAskEach));
         }
     }
     if (!url.isEmpty()) {
