cdbc7c964653d20085fead3b4d0d367f8b3fb8d4
Yuliya Algaer <yalgaer@gmail.com>
2022-07-25 08:41:37 +0700

    Improve UI tests distribution per suite on Windows

--- src/plugins/GUITestBase/src/harness/GUITestLauncher.cpp
+++ src/plugins/GUITestBase/src/harness/GUITestLauncher.cpp
@@ -135,7 +135,7 @@ void GUITestLauncher::firstTestRunCheck(const QString& testName) {
 QList<GUITest*> getIdealNightlyTestsSplit(int suiteIndex, int suiteCount, const QList<GUITest*>& allTests) {
     QList<int> testsPerSuite;
     if (suiteCount == 3) {  // Windows.
-        testsPerSuite << 950 << 840 << -1;
+        testsPerSuite << 920 << 880 << -1;
     } else if (suiteCount == 4) {
         testsPerSuite << 640 << 680 << 640 << -1;
     } else if (suiteCount == 5) {
