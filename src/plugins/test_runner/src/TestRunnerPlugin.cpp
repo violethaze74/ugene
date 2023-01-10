1f39f5e91b302f103b2c339e2908a9a002e54ff9
Yuliya Algaer <yalgaer@gmail.com>
2023-01-11 02:51:09 +0700

    Set environment fix (#1082)

--- src/plugins/test_runner/src/TestRunnerPlugin.cpp
+++ src/plugins/test_runner/src/TestRunnerPlugin.cpp
@@ -257,6 +257,8 @@ GTestSuite* TestRunnerService::findTestSuiteByURL(const QString& url) {
 void TestRunnerService::readBuiltInVars() {
     QStringList biVars;
     biVars << NUM_THREADS_VAR;
+    biVars << "COMMON_DATA_DIR";
+    biVars << "TEMP_DATA_DIR";
     readEnvForKeys(biVars);
 
     QMap<QString, QString> vars = env->getVars();
